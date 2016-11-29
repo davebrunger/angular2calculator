22bb74e34a7e91942b04fd2ff9519c3da19b2cec
David Brunger <david.brunger@alpha9software.com>
2016-11-29 10:21:16 +0000

    Support numeric input from clicking buttons

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,6 +4,38 @@ import { Component } from '@angular/core';
   selector: 'my-app',
   templateUrl: './app/app.component.html',
 })
-export class AppComponent  {
-  result = "0";
+export class AppComponent {
+  private result = 0;
+  private display: string;
+  private input = "";
+
+  constructor() {
+    this.setDisplay(0);
+  }
+
+  private setDisplay(newValue: number) {
+    if (newValue) {
+      this.display = newValue.toString();
+      if (this.display.indexOf(".") < 0) {
+        this.display = this.display + ".";
+      }
+    }
+    else {
+      this.display = "0."
+    }
+  }
+
+  public digitClicked(event: MouseEvent) {
+    this.input = this.input + event.toElement.innerHTML;
+    this.setDisplay(parseFloat(this.input));
+    event.preventDefault();
+  }
+
+  public decimalClicked() {
+    if (this.input.indexOf(".") < 0) {
+      this.input = this.input + ".";
+    }
+    this.setDisplay(parseFloat(this.input));
+    event.preventDefault();
+  }
 }
