13d1a9837572ec1ab95fc3ecbcb858ac9cab3c23
David Brunger <david.brunger@alpha9software.com>
2016-11-29 09:23:51 +0000

    Add Bootstrap support

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,12 @@
-import { NgModule }      from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
-import { AppComponent }  from './app.component';
+import { AppComponent } from './app.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [NgbModule.forRoot(), BrowserModule],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
